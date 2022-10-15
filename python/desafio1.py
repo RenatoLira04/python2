652d6294d6fc6e896cd1e9a2f94bd3d6815d196c
Renato Lira <renato.lira.almeida@gmail.com>
2022-10-14 21:10:20 -0300

    comitar tudo

---
+++ python/desafio1.py
@@ -0,0 +1,4 @@
+nome=input('Qual é seu nome?')
+mensagem1= 'Olá'
+mensagem2= '! Prazer em te conhecer!'
+print(mensagem1,nome,mensagem2)
---
+++ python/desafio2.py
@@ -0,0 +1,8 @@
+dia=input('Dia=')
+mes=input('Mês=')
+ano=input('Ano=')
+mensagem1='Você nasceu no dia'
+mensagem2='de'
+mensagem3='de'
+mensagem4='. Correto?'
+print (mensagem1, dia, mensagem2, mes, mensagem3, ano, mensagem4)
